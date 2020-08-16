acb62e18ea780fcd2f42860772942508669cd3f9
Giovanni Vincenti <giovanni.vincenti@gmail.com>
2020-08-15 23:22:13 -0400

    Add files via upload

---
+++ create_new_file.py
@@ -0,0 +1,14 @@
+from krita import *
+
+# I create a new document of size 1600x1200
+newDoc = Krita.instance().createDocument(1600, 1200, "Test", "RGBA", "U8", "", 120.0)
+
+# The new document has a transparent background, and here I set it to white (so I can write on it)
+newDoc.setBackgroundColor(QColor(255, 255, 255, 255))
+
+# I want to save the document right away, but it is not working yet
+newDoc.saveAs("/home/ubprof/Desktop/test.kra")
+# newDoc.save()
+
+# The new document is added to the active window
+Krita.instance().activeWindow().addView(newDoc)
